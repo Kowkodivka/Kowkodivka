108d706ff8597601a03f505c44a507d6d1a9aecd
Kowkodivka <86189625+Kowkodivka@users.noreply.github.com>
2025-08-01 15:03:04 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export const App = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <p className="text-2xl">Hello, World</p>
+      </div>
+    </>
+  );
+};
