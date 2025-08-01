76c8899a07f36201efeed3352446c0235e79b519
Kowkodivka <86189625+Kowkodivka@users.noreply.github.com>
2025-08-01 16:23:55 +0700

    Bio + gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import solid from "vite-plugin-solid";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "Kowkodivka",
   plugins: [solid(), tailwindcss()],
 });
